cb7233b8ab7a3015246b2e72ac2d233582ccafe2
lgants <lgants@gmail.com>
2016-09-21 15:29:31 -0400

    js object methods lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code solution here
+class President {
+  constructor(name, politicalParty, yearsInOffice, homeState){
+    this.name = name;
+    this.politicalParty = politicalParty;
+    this.yearsInOffice = yearsInOffice;
+    this.homeState = homeState;
+  }
+  veto(){
+    return "NO!";
+  }
+  passBill(){
+    return "You can do that!";
+  }
+  doCharity(){
+    return "I like to help people.";
+  }
+  conductPressInterview(){
+    return "I am proud to be an American.";
+  }
+  sayHi(){
+    return "Hi, my name is " + this.name + ". I am from " + this.homeState + ". I represent the " + this.politicalParty + "s, and was in office " + this.yearsInOffice + ".";
+  }
+}
+
+var george_washington = new President("George Washington");
+
+var abraham_lincoln = new President("Abraham Lincoln");
+
+var richard_nixon = new President("Richard Nixon");
+
+var jimmy_Carter = new President("Jimmy Carter");
